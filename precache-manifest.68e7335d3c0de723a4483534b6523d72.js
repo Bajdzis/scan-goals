f8854b1df25613e593ddeb9465c0521449e30917
Bajdzis <rafal612b@wp.pl>
2019-11-13 21:24:47 +0100

    Updates

---
+++ precache-manifest.68e7335d3c0de723a4483534b6523d72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9841999dfb0d6b35aa148f3b9f39f73c",
+    "url": "/scan-goals/index.html"
+  },
+  {
+    "revision": "438f6f8f055aaa329928",
+    "url": "/scan-goals/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "6fa5dba6fffa989b1074",
+    "url": "/scan-goals/static/js/2.20361c3e.chunk.js"
+  },
+  {
+    "revision": "438f6f8f055aaa329928",
+    "url": "/scan-goals/static/js/main.6a833f7c.chunk.js"
+  },
+  {
+    "revision": "12640ac3f3d731f1df24",
+    "url": "/scan-goals/static/js/runtime-main.cab9431f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/scan-goals/static/media/logo.25bf045c.svg"
+  }
+]);
