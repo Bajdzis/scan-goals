c2dd691385d1d47bb68ad41892e136f6a4413cc5
Bajdzis <rafal612b@wp.pl>
2019-11-18 21:31:15 +0100

    Updates

---
+++ precache-manifest.6c5288fa6a81d2bb4cd7675c8649a414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e6ce49906a02752e04548102780f85",
+    "url": "/scan-goals/index.html"
+  },
+  {
+    "revision": "d7ae16f08a5188987cb1",
+    "url": "/scan-goals/static/css/main.b3bfbe43.chunk.css"
+  },
+  {
+    "revision": "908a3c94dfdc7d02e413",
+    "url": "/scan-goals/static/js/2.7b38d3b5.chunk.js"
+  },
+  {
+    "revision": "d7ae16f08a5188987cb1",
+    "url": "/scan-goals/static/js/main.62941f66.chunk.js"
+  },
+  {
+    "revision": "12640ac3f3d731f1df24",
+    "url": "/scan-goals/static/js/runtime-main.cab9431f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scan-goals/precache-manifest.4d9756d13734b52a81a097e0b5887b8f.js"
+  "/scan-goals/precache-manifest.6c5288fa6a81d2bb4cd7675c8649a414.js"
 );
 
 self.addEventListener('message', (event) => {
